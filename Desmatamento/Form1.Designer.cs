ddef54239a9e0cd452272a836080536418ca87e9
RevenantParagon <brunocosta176186@gmail.com>
2019-12-11 21:27:22 -0300

    Upload Desmatamento

---
+++ Desmatamento/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Desmatamento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Selecionar Imagem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(353, 385);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(391, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(353, 385);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(397, 400);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(397, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 480);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Desmatamento/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DIPLi;
+
+namespace Desmatamento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Imagem imagem;
+
+        private object Abrir()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            openFileDialog.Title = "Selecionar Imagem";
+            openFileDialog.InitialDirectory = "C:\\";
+            openFileDialog.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" + "All files (*.*)|*.*";
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.ReadOnlyChecked = true;
+            openFileDialog.ShowReadOnly = true;
+            openFileDialog.FileName = "";
+
+            DialogResult dr = openFileDialog.ShowDialog();
+            Imagem image;
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = openFileDialog.FileName.ToString();
+                image = new Imagem(file);
+                return image;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            imagem = (Imagem)Abrir();
+            Convolucao();
+            pictureBox1.Image = imagem.ToBitmap();
+            pictureBox2.Image = Percorrer().ToBitmap();
+        }
+
+        public void Convolucao()
+        {
+            Imagem R = imagem;
+            int[,] W = new int[,] { { 1, 1, 1, 1, 1 },
+                                    { 1, 1, 1, 1, 1 },
+                                    { 1, 1, 1, 1, 1 },
+                                    { 1, 1, 1, 1, 1 },
+                                    { 1, 1, 1, 1, 1}};
+
+            for (int i = 2; i < R.Altura - 2; i++)
+            {
+                for (int j = 2; j < R.Largura - 2; j++)
+                {
+                    for (int c = 0; c < 3; c++)
+                    {
+                        R[i, j, c] = (W[0, 0] * R[i + 2, j - 2, c] + W[0, 1] * R[i + 2, j - 1, c] + W[0, 2] * R[i + 2, j, c] +
+                             W[0, 3] * R[i + 2, j + 1, c] + W[0, 4] * R[i + 2, j + 2, c] + W[1, 0] * R[i + 1, j - 2, c] +
+                             W[1, 1] * R[i + 1, j - 1, c] + W[1, 2] * R[i + 1, j, c] + W[1, 3] * R[i + 1, j + 1, c] +
+                             W[1, 4] * R[i + 1, j + 2, c] + W[2, 0] * R[i, j - 2, c] + W[2, 1] * R[i, j - 1, c] +
+                             W[2, 2] * R[i, j, c] + W[2, 3] * R[i, j + 1, c] + W[2, 4] * R[i, j + 2, c] + W[3, 0] * R[i - 1, j - 2, c] +
+                             W[3, 1] * R[i - 1, j - 1, c] + W[3, 2] * R[i - 1, j, c] + W[3, 3] * R[i - 1, j + 1, c] +
+                             W[3, 4] * R[i - 1, j + 2, c] + W[4, 0] * R[i - 2, j - 2, c] + W[4, 1] * R[i - 2, j - 1, c] +
+                             W[4, 2] * R[i - 2, j, c] + W[4, 3] * R[i - 2, j + 1, c] + W[4, 4] * R[i - 2, j + 2, c]) / 25;
+                    }                    
+                }
+            }
+            imagem = R;
+        }
+
+        private Imagem Percorrer()
+        {
+            int d = 0;
+            int m = 0;
+            float p = 0;
+
+            Imagem img = new Imagem(imagem.Largura,imagem.Altura, imagem.Tipo);
+
+            for (int l = 0; l < imagem.Largura; l++)
+            {
+                for (int a = 0; a < imagem.Altura; a++)
+                {
+                    if (imagem[a, l, 0] > imagem[a, l, 1])
+                    {
+                        img[a, l, 0] = 255;
+                        d++;
+                    }
+                    else
+                    {
+                        img[a, l, 0] = imagem[a, l, 0];
+                        img[a, l, 1] = imagem[a, l, 1];
+                        img[a, l, 2] = imagem[a, l, 2];
+                        m++;
+                    }
+                }
+            }
+            p = (d * 100) / (d + m);
+            label1.Text = "A porcentagem da área desmatada é de: " + p.ToString() + "%";
+
+            Imagem mascara = new Imagem(img.Largura,img.Altura,TipoImagem.Monocromatica);
+
+            for (int l = 0; l < img.Largura; l++)
+            {
+                for (int a = 0; a < img.Altura; a++)
+                {
+                    if (img[a,l,0] == 255 && mascara[a, l, 1] == 0 && mascara[a, l, 2] == 0)
+                    {
+                        mascara[a, l] = 0;
+                    }
+                    else
+                    {
+                        mascara[a, l] = 255;
+                    }
+                }
+            }
+
+            mascara.ToGrayscale();
+            Imagem carregada = (Imagem)Abrir();
+            carregada.ToGrayscale();
+
+            int c = 0;
+            int e = 0;
+            int por = 0;
+
+            for (int l = 0; l < mascara.Largura; l++)
+            {
+                for (int a = 0; a < mascara.Altura; a++)
+                {
+                    if (carregada[a, l] == 0 && mascara[a, l] == 0)
+                    {
+                        c++;
+                    }
+                    else if (carregada[a, l] == 0 && mascara[a, l] != 0)
+                    {
+                        e++;
+                    }
+                }
+            }
+
+            por = (c * 100) / (c + e);
+
+            label2.Text = "A taxa de acerto do algoritimo é de: " + por.ToString() + "%";
+
+            return img;
+        }
+    }
+}
